28a977ef7186bde44957ed8d8a8953ab9dce50a9
mahdihaghnegar <mahdi.haghnegar@gmail.com>
2024-05-12 22:38:59 +0330

    client first step

---
+++ client/src/components/First.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+const First = () => {
+  return <p>home</p>;
+};
+
+export default First;
---
+++ client/src/main.jsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Record from "./components/Record";
+import First from "./components/First";
+import Login from "./components/Login";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "/edit/:id",
+    element: <App />,
+    children: [
+      {
+        path: "/edit/:id",
+        element: <Record />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <First />,
+      },
+    ],
+  },
+  // {
+  //   path: "/create",
+  //   element: <App />,
+  //   children: [
+  //     {
+  //       path: "/create",
+  //       element: <Create />,
+  //     },
+  //   ],
+  // },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+/*import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)*/
