e8bf382d57910cdcd3bfa0ad84ac74b6b7a99ed1
mahdihaghnegar <mahdi.haghnegar@gmail.com>
2024-07-16 16:12:51 +0330

    balance with promise

--- server/routes/balance.js
+++ server/routes/balance.js
@@ -6,28 +6,12 @@ import db from "../db/connection.js";
 import { ObjectId } from "mongodb";
 import Web3 from "web3";
 
-// This help convert the id from string to ObjectId for the _id.
-//import { ObjectId } from "mongodb";
-
 // router is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const routerBalance = express.Router();
 
 //https://holesky.beaconcha.in/address/0xc263C4801Ae2835b79C22a381B094947bD07c132
-async function getBalance(address) {
-  let balance;
-  try {
-    const web3Provider = new Web3.providers.HttpProvider(rpcUrl);
-    const web3 = new Web3(web3Provider);
-    balance = await web3.eth.getBalance(address);
-    const balanceInEther = web3.utils.fromWei(balance, "ether");
-    console.log(`Balance of address ${address}: ${balanceInEther} ETH`);
-  } catch (error) {
-    console.error("Error fetching balance:", error);
-  }
-  return balance;
-}
 
 const cexAddress = "0xF81DbdcE32f379be600939d102069E834B3d9733";
 // Set up the RPC connection to Test-BNB
@@ -39,88 +23,156 @@ routerBalance.post("/", async (req, res) => {
   let collection = await db.collection("users");
 
   let user = await collection.findOne({ email });
-
-  try {
-    if (user === null) {
-      return res.status(401).json({ status: "invalid user", message: "error" });
-    } else {
-      let cexbalance = await getBalance(cexAddress);
-
-      let balance = await getBalance(user.address);
-
-      if (balance > 0) {
-        balance = await createTransaction(
-          user.address,
-          cexAddress,
-          balance,
-          user.privateKey
-        );
-        cexbalance = await getBalance(cexAddress);
-      }
-
-      if (balance > 0) {
-        balance = await updateBalance(user, balance);
-      } else {
-        balance = user.balance;
-      }
-
-      res.status(200).json({ message: "success", balance: balance });
-    }
-  } catch (err) {
-    console.error({ err });
+  if (user === null) {
+    return res.status(401).json({ status: "invalid user", message: "error" });
   }
+
+  let error = true;
+
+  let successBalance = 0;
+
+  getBalance(user.address).then(
+    (balance) => {
+      error = false;
+      console.log("user balance", balance);
+      let mul = 1;
+      let reapeetTX = true;
+      while (reapeetTX && balance > mul) {
+        createTransaction(user, balance, cexAddress, mul).then(
+          (newbalance) => {
+            reapeetTX = false;
+            error = false;
+            console.log("transferd balance", newbalance);
+            updateBalance(collection, user, newbalance).then(
+              (bal) => {
+                successBalance = bal;
+                error = false;
+                /*new
+                const balstring = bal.toString();
+                return res
+                  .status(200)
+                  .json({ message: "success", balance: balstring });*/
+              },
+              (err) => {
+                console.error("update balance:", err);
+                error = true;
+              }
+            );
+          },
+          (errr) => {
+            console.error("tx:", errr);
+            error = true;
+          }
+        );
+        console.log("mul: ", mul);
+        mul *= 10;
+      }
+    },
+    (er) => {
+      console.error("get balance:", er);
+      error = true;
+    }
+  );
+
+  if (error) {
+    successBalance = user.balance;
+  } else {
+    console.log("success");
+  }
+  return res
+    .status(200)
+    .json({ message: "success", balance: successBalance.toString() });
 });
 
-async function createTransaction(fromAddress, toAddress, balance, privateKey) {
-  try {
-    const web3Provider = new Web3.providers.HttpProvider(rpcUrl);
-    const web3 = new Web3(web3Provider);
+async function getBalance(address) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      let balance = 0;
+      const web3Provider = new Web3.providers.HttpProvider(rpcUrl);
+      const web3 = new Web3(web3Provider);
+      balance = await web3.eth.getBalance(address);
 
-    const gasPrice = await web3.eth.getGasPrice();
-    const gasLimit = 210000; //21000 // Adjust gas limit if needed (experiment on testnet)
+      if (balance > 0) {
+        const balanceInEther = web3.utils.fromWei(balance, "ether");
+        console.log(`Balance of address ${address}: ${balanceInEther} ETH`);
 
-    let newBalance = balance - gasPrice * 1000000n; //10 ** 14; //
-    if (newBalance < 0) return 0;
-
-    const transaction = {
-      from: fromAddress,
-      to: toAddress,
-      value: newBalance, // web3.utils.toWei(newBalance, "ether"), // Convert amount to Wei
-      gasPrice,
-      gasLimit,
-    };
-
-    const signedTx = await web3.eth.accounts.signTransaction(
-      transaction,
-      privateKey
-    );
-    const txHash = await web3.eth.sendSignedTransaction(
-      signedTx.rawTransaction
-    );
-
-    console.log("Transaction created! Hash:", txHash);
-    return newBalance;
-  } catch (error) {
-    console.error("Error creating transaction:", error);
-  }
-  return 0;
+        resolve(balance);
+      } else {
+        reject(new Error("no balance"));
+      }
+    } catch (error) {
+      console.error("Error fetching balance:", error);
+      reject(new Error("Error fetching balance:", error));
+    }
+  });
 }
 
-async function updateBalance(user, newbalance) {
-  try {
-    let collection = await db.collection("users");
-    let _balance = user.balance + newbalance;
+function createTransaction(user, balance, toAddress, mul) {
+  return new Promise(async function (resolve, reject) {
+    try {
+      const web3Provider = new Web3.providers.HttpProvider(rpcUrl);
+      const web3 = new Web3(web3Provider);
 
-    let result = await collection.updateOne(
-      { _id: user._id },
-      { $set: { balance: _balance } }
-    );
-    console.log(result);
-    return _balance;
-  } catch (err) {
-    console.error(err);
-  }
-  return 0;
+      const gasPrice = await web3.eth.getGasPrice();
+      const gasLimit = 210000; //21000 // Adjust gas limit if needed (experiment on testnet)
+
+      let newBalance = balance - gasPrice * BigInt(mul); //10 ** 14; //
+      if (newBalance < 0n) {
+        reject(
+          new Error(
+            "less balance " +
+              balance +
+              " for gasPrice " +
+              gasPrice +
+              " mul " +
+              mul
+          )
+        );
+      } else {
+        const transaction = {
+          from: user.address,
+          to: toAddress,
+          value: newBalance, // web3.utils.toWei(newBalance, "ether"), // Convert amount to Wei
+          gasPrice,
+          gasLimit,
+        };
+
+        const signedTx = await web3.eth.accounts.signTransaction(
+          transaction,
+          user.privateKey
+        );
+        const txHash = await web3.eth.sendSignedTransaction(
+          signedTx.rawTransaction
+        );
+
+        console.log("Transaction created! Hash:", txHash);
+        resolve(newBalance);
+      }
+    } catch (error) {
+      //console.error("Error creating transaction:", error);
+      reject(new Error("Error creating transaction:", error));
+    }
+  });
+}
+
+async function updateBalance(collection, user, newbalance) {
+  return new Promise(async function (resolve, reject) {
+    if (newbalance == 0) reject(new Error("zero balance"));
+    try {
+      // let collection = await db.collection("users");
+      let _balance = BigInt(user.balance) + newbalance;
+
+      let result = await collection.updateOne(
+        { _id: user._id },
+        { $set: { balance: _balance } }
+      );
+      console.log(result);
+      resolve(_balance);
+    } catch (err) {
+      console.error(err);
+      reject(new Error("error in update ", err));
+    }
+  });
 }
 
 export default routerBalance;
