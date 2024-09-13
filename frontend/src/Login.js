e0b0d81abf764e3c34cdf221b42a3099d1fedd92
mahdihaghnegar <mahdi.haghnegar@gmail.com>
2024-09-13 21:26:16 +0330

    login ok

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -23,7 +23,7 @@ const Login = (props) => {
       return;
     }
 
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    if (!/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
       setEmailError("Please enter a valid email");
       return;
     }
@@ -104,10 +104,18 @@ const Login = (props) => {
 
   return (
     <div className="login-container">
+      <div className="header">
+        <div className="url-bar">http://cex-fr.onrender.com/login/</div>
+        <div className="browser-controls">
+          <button className="red"></button>
+          <button className="orange"></button>
+          <button className="green"></button>
+        </div>
+      </div>
       <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
+        <h2>ورود به صرافی غیر متمرکز مهدی</h2>
         <div className="input-group">
-          <label>Email</label>
+          <label> ايميل</label>
           <input
             type="email"
             value={email}
@@ -117,80 +125,19 @@ const Login = (props) => {
           <label className="errorLabel">{emailError}</label>
         </div>
         <div className="input-group">
-          <label>Password</label>
+          <label>رمز عبور</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-          />{" "}
+          />
           <label className="errorLabel">{passwordError}</label>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit">ورود</button>
       </form>
     </div>
   );
 };
 
 export default Login;
-/*import { useState } from "react";
-
-const Login = () => {
-  const [input, setInput] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleSubmitEvent = (e) => {
-    e.preventDefault();
-    if (input.username !== "" && input.password !== "") {
-      //dispatch action from hooks
-    }
-    alert("please provide a valid input");
-  };
-
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setInput((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  return (
-    <form onSubmit={handleSubmitEvent}>
-      <div className="form_control">
-        <label htmlFor="user-email">Email:</label>
-        <input
-          type="email"
-          id="user-email"
-          name="email"
-          placeholder="example@yahoo.com"
-          aria-describedby="user-email"
-          aria-invalid="false"
-          onChange={handleInput}
-        />
-        <div id="user-email" className="sr-only">
-          Please enter a valid username. It must contain at least 6 characters.
-        </div>
-      </div>
-      <div className="form_control">
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          aria-describedby="user-password"
-          aria-invalid="false"
-          onChange={handleInput}
-        />
-        <div id="user-password" className="sr-only">
-          your password should be more than 6 character
-        </div>
-      </div>
-      <button className="btn-submit">Submit</button>
-    </form>
-  );
-};
-
-export default Login;*/
