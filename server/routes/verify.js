8fec766c37f9118f41bf5569f7f805c4a42cd829
mahdiRMS <mahdi.haghnegar@gmail.com>
2024-10-05 14:17:49 +0330

    swagger Verify JWT

--- server/routes/verify.js
+++ server/routes/verify.js
@@ -2,20 +2,60 @@ import express from "express";
 
 import jwt from "jsonwebtoken";
 
-// This help convert the id from string to ObjectId for the _id.
-//import { ObjectId } from "mongodb";
-
-// router is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /record.
 const routerVerify = express.Router();
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     VerifyResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: string
+ *           description: The status of the verification
+ *         message:
+ *           type: string
+ *           description: The message indicating success or error
+ */
 
-// The verify endpoint that checks if a given JWT token is valid
+/**
+ * @swagger
+ * /verify:
+ *   post:
+ *     summary: Verify if a given JWT token is valid
+ *     tags: [Verify JWT]
+ *     parameters:
+ *       - in: header
+ *         name: jwt-token
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The JWT token to verify
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/VerifyResponse'
+ *       401:
+ *         description: Invalid auth
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/VerifyResponse'
+ */
+
+/**
+ * POST /verify
+ * Endpoint to check if a given JWT token is valid.
+ * @param {string} jwt-token - The JWT token to verify, provided in the request headers.
+ * @returns {object} - A JSON object indicating the verification status.
+ */
 routerVerify.post("/", (req, res) => {
-  const tokenHeaderKey = "jwt-token";
-  const authToken = req.headers[tokenHeaderKey];
-  //const JWTSecretKey = process.env.JWTSecretKey || "";
-  const JWTSecretKey = "dsfdsfsdfdsvcsvdfgefg";
+  const tokenHeaderKey = "jwt-token"; // The key for the JWT token in the request headers
+  const authToken = req.headers[tokenHeaderKey]; // Retrieve the JWT token from the request headers
+  const JWTSecretKey = process.env.JWTSecretKey || "dsfdsfsdfdsvcsvdfgefg"; // The secret key used to verify the JWT token
 
   try {
     const verified = jwt.verify(authToken, JWTSecretKey);
